436709887e88cc81707d070215fd223f8e2cedd6
Ivan Chromjak <ivan@chromjak.com>
2022-12-16 17:11:53 -0500

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
     },
   },
   plugins: [
-    // require('@tailwindcss/forms'),
+    require('@tailwindcss/forms'),
     // require('@tailwindcss/aspect-ratio'),
     // require('@tailwindcss/typography'),
   ]
